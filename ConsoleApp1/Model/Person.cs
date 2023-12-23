92e2a849227616553f7e2c0f12969ca641b7e190
Makara <makaragit.sam@2023.com>
2023-12-23 20:19:06 +0700

    Inheritance

---
+++ ConsoleApp1/Model/Person.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Model
+{
+    public  class Person
+    {
+        public int Id  { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int CardId { get; set; }
+        public string TelePhone { get; set; }
+        public string Address { get; set; }
+    
+        public Person()
+        {
+            Id = 0;
+            Name = "null";
+            Gender = "null";
+            CardId = 0;
+            TelePhone = "null";
+            Address = "null";
+        }
+        public void Input()
+        {
+            Console.WriteLine(">>>>>>>>>>>Input your information detail>>>>>>>>>");
+            Console.Write("Input Id:");
+            Id = int.Parse(Console.ReadLine());
+            Console.Write("Input Name:");
+            Name = Console.ReadLine();
+            Console.Write("Input Gender(Male/Female):");
+            Gender = Console.ReadLine();
+            Console.Write("Input CardId:");
+            CardId = int.Parse(Console.ReadLine());
+            Console.Write("Input Phone Number:");
+            TelePhone = Console.ReadLine();
+            Console.Write("Input Address :");
+            Address = Console.ReadLine();
+
+        }
+        public void Output()
+        {
+            Console.WriteLine("This is imformation Detail ");
+            Console.WriteLine($"Id : {Id}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Gender: {Gender}");
+            Console.WriteLine($"CardId: {CardId}");
+            Console.WriteLine($"Phone: {TelePhone}");
+            Console.WriteLine($"Address: {Address}");
+             
+        }
+        
+    }
+    
+    class Employee: Person
+    {
+        public double Salary {  get; set; }
+        public string Role { get; set; }
+        public double Bonus { get; set; }
+        public void Input()
+        {
+            base.Input();
+            Console.Write("Input Salary:");
+            Salary = double.Parse(Console.ReadLine());
+            Console.Write("Input Role:");
+            Role = Console.ReadLine();
+            Console.Write("Input Bounus:");
+            Bonus = double.Parse(Console.ReadLine());
+        }
+        public void Ouput()
+        {
+            base.Output();
+            Console.WriteLine($"Salary:{Salary}");
+            Console.WriteLine($"Role:{Role}");
+            Console.WriteLine($"Bounus:{Bonus}");
+        }
+    }
+    class Teacher:Person
+    {
+        public string Subject { get; set; }
+        public double Salary { get; set; }
+        public string Role { get; set; }
+        public double Bonus { get; set; }
+
+        public void Input()
+        {
+            base.Input();
+            Console.Write("Input Subject:");
+            Subject = Console.ReadLine();
+            Console.Write("Input Salary:");
+            Salary = double.Parse(Console.ReadLine());
+            Console.Write("Input Role:");
+            Role = Console.ReadLine();
+            Console.Write("Input Bounus:");
+            Bonus = double.Parse(Console.ReadLine());
+        }
+        public void Output()
+        {
+            base.Output();
+            Console.WriteLine($"Subject: {Subject}");
+            Console.WriteLine($"Salary:{Salary}");
+            Console.WriteLine($"Role:{Role}");
+            Console.WriteLine($"Bounus:{Bonus}");
+        }
+    }
+
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleApp1.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] people = new Person[2];
+
+            for(int i = 0; i < people.Length; i++)
+            {
+                Employee employee = new Employee();
+                employee.Input();
+                people[i] = employee;
+            }
+            Teacher teacher = new Teacher();
+            teacher.Input();
+            people[2] = teacher;
+
+            foreach(Person person in people)
+            {
+                Console.WriteLine("Person Informations\n");
+                person.Output();
+                Console.WriteLine("----------------------");
+            }
+        }
+    }
+}
